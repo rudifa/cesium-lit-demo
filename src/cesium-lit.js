9e6ed576f2cadabe46982380d7fa49d7610c5481
Rudi Farkas <rudi.farkas@gmail.com>
2022-11-13 17:19:16 +0100

    tidy up code, names and comments

--- src/cesium-lit.js
+++ src/cesium-lit.js
@@ -4,46 +4,17 @@ import {html, css, unsafeCSS, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
 import * as Cesium from 'cesium';
-// 1
-// not knowing how to import the widgets.css file, I just copy it into widgets.js
-// and added the code to export the styles as css`...`
-// it works
-import {widgetStyles} from './widgets.js';
 
-// 2
-// This also works, tested in dev and in build+preview
+// This works, tested in dev and in build+preview
 import widgetStylesRaw from 'cesium/Build/Cesium/Widgets/widgets.css';
-const widgetStyles2 = css`
+const widgetStyles = css`
   ${unsafeCSS(widgetStylesRaw)}
 `;
 
-console.log(`widgetStylesRaw = ${widgetStylesRaw}`);
-console.log(` widgetStyles2 = `, widgetStyles2);
-// ^^^ is a CSSResult, looks good ^^^
-
 // Your access token can be found at: https://cesium.com/ion/tokens.
-// This is the default access token from your ion account
-
+// This is the default access token from the cesiumjs-quickstart demo
 Cesium.Ion.defaultAccessToken =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwMDY5YjNjNy05ZDZjLTQ5YjUtODBhOC03MGY4Njc3MzUyMDEiLCJpZCI6MTEyNTc3LCJpYXQiOjE2NjY4MTYyNjB9.fd9TA4pMsDaKBWE1lSEBvYB34xR-R1anLfSG-vSVI4c';
-
-// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-// const viewer = new Cesium.Viewer('cesiumContainer', {
-//   terrainProvider: Cesium.createWorldTerrain(),
-// });
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-// const buildingTileset = viewer.scene.primitives.add(
-//   Cesium.createOsmBuildings()
-// );
-
-// Fly the camera to Geneva at the given longitude, latitude, and height.
-// viewer.camera.flyTo({
-//   destination : Cesium.Cartesian3.fromDegrees(6.14569, 46.20222, 200000),
-//   orientation : {
-//     heading : Cesium.Math.toRadians(0.0),
-//     pitch : Cesium.Math.toRadians(-15.0),
-//   }
-// });
 
 /**
  * Clamp the value to the range [min, max].
@@ -99,15 +70,13 @@ class CameraCoordinates {
 export class CesiumLit extends LitElement {
   static get styles() {
     return [
-      widgetStyles2,
+      widgetStyles,
       css`
       :host {
-        /* display: block; no effect */
-        border: solid 1px red;
-        background: pink;
+        display: block; /* no effect */
         padding: 10px;
         max-width: 100vw;
-        max-height: 100vw; */ /*no effect*/
+        max-height: 100vw; /*no effect*/
         font-size: 16px;
         font-family: Helvetica, Arial, sans-serif;
         /* box-sizing: content-box; no effect */
@@ -115,7 +84,7 @@ export class CesiumLit extends LitElement {
       button {
         font-size: 14px;
       }
-      #cesiumContainer2 {
+      #cesiumContainer {
         /* width: 50%; */
         /* width: 100%; */
         /* height: 100%; */ /*does not prevent vertical growth*/
@@ -126,10 +95,12 @@ export class CesiumLit extends LitElement {
         /* height: 700px;*/ /*prevents vertical growth */
         background: thistle;
         /* overflow: hidden; not needed */
+        border: solid 1px blue;
       }
-      #separator {
-        width: 100%;
-        background: skyblue;
+      .lit-widgets {
+        background: hsl(0, 0%, 90%);
+        border: solid 1px blue;
+        padding: 10px;
       }
     `,
     ];
@@ -144,30 +115,23 @@ export class CesiumLit extends LitElement {
   }
 
   firstUpdated() {
-    const cesiumContainer2 = this.renderRoot.getElementById('cesiumContainer2');
-    console.log(`firstUpdated cesiumContainer2:`, cesiumContainer2);
-    this.viewer = new Cesium.Viewer(cesiumContainer2, {
+    const cesiumContainer = this.renderRoot.getElementById('cesiumContainer');
+    console.log(`firstUpdated cesiumContainer:`, cesiumContainer);
+    this.viewer = new Cesium.Viewer(cesiumContainer, {
       terrainProvider: Cesium.createWorldTerrain(),
-      // cesiumWidget: true,
-      // infoBox: true,
     });
-    // this.viewer.forceResize();
     this.flyTo();
   }
 
   render() {
     return html`
-
-      <h3>Hello, ${this.name}!</h3>
-      <div>
+      <div class="lit-widgets">
+        <h3>Hello, ${this.name}!</h3>
         <button id="--" @click=${this._onClick} part="button">--</button>
         <button id="++" @click=${this._onClick} part="button">++</button>
-        height: ${this.cameraCoordinates.height}
+        &nbsp; height: ${this.cameraCoordinates.height} m
       </div>
-      <div id="separator">div 1</div>
-      <div id="cesiumContainer2">div 2: #cesiumContainer2</div>
-      <div id="separator"">div 3</div>
- 
+      <div id="cesiumContainer"></div>
     `;
   }
 
