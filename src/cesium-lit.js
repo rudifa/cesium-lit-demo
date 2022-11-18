36e54f54ac0003a9b8033c34b94daa99d0915c8f
Rudi Farkas <rudi.farkas@gmail.com>
2022-11-18 16:01:25 +0100

    move creditDisplay etc styles into src/cesium-credit-display.css

--- src/cesium-lit.js
+++ src/cesium-lit.js
@@ -6,10 +6,16 @@ import {customElement} from 'lit/decorators.js';
 import * as Cesium from 'cesium';
 
 // This works, tested in dev and in build+preview
-import widgetStylesRaw from 'cesium/Build/Cesium/Widgets/widgets.css';
-const widgetStyles = css`
-  ${unsafeCSS(widgetStylesRaw)}
+import widgetCssRaw from 'cesium/Build/Cesium/Widgets/widgets.css';
+const widgetsCss = css`
+  ${unsafeCSS(widgetCssRaw)}
 `;
+
+import creditDisplayCssRaw from './cesium-credit-display.css';
+const creditDisplayCss = css`
+  ${unsafeCSS(creditDisplayCssRaw)}
+`;
+
 
 // Your access token can be found at: https://cesium.com/ion/tokens.
 // This is the default access token from the cesiumjs-quickstart demo
@@ -71,7 +77,8 @@ class CameraCoordinates {
 export class CesiumLit extends LitElement {
   static get styles() {
     return [
-      widgetStyles,
+      widgetsCss,
+      creditDisplayCss,
       css`
         :host {
           display: block; /* no effect */
@@ -97,118 +104,7 @@ export class CesiumLit extends LitElement {
           padding: 10px;
         }
 
-        /* add styles for the credit-lightbox overlay */
-
-        .cesium-credit-lightbox-overlay {
-          display: none;
-          z-index: 1;
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          background-color: rgba(80, 80, 80, 0.8);
-        }
-        .cesium-credit-lightbox {
-          background-color: #303336;
-          color: #ffffff;
-          position: relative;
-          min-height: 100px;
-          margin: auto;
-        }
-        .cesium-credit-lightbox > ul > li a,
-        .cesium-credit-lightbox > ul > li a:visited {
-          color: #ffffff;
-        }
-        .cesium-credit-lightbox > ul > li a:hover {
-          color: #48b;
-        }
-        .cesium-credit-lightbox.cesium-credit-lightbox-expanded {
-          border: 1px solid #444;
-          border-radius: 5px;
-          max-width: 370px;
-        }
-        .cesium-credit-lightbox.cesium-credit-lightbox-mobile {
-          height: 100%;
-          width: 100%;
-        }
-        .cesium-credit-lightbox-title {
-          padding: 20px 20px 0 20px;
-        }
-        .cesium-credit-lightbox-close {
-          font-size: 18pt;
-          cursor: pointer;
-          position: absolute;
-          top: 0;
-          right: 6px;
-          color: #ffffff;
-        }
-        .cesium-credit-lightbox-close:hover {
-          color: #48b;
-        }
-        .cesium-credit-lightbox > ul {
-          margin: 0;
-          padding: 12px 20px 12px 40px;
-          font-size: 13px;
-        }
-        .cesium-credit-lightbox > ul > li {
-          padding-bottom: 6px;
-        }
-        .cesium-credit-lightbox > ul > li * {
-          padding: 0;
-          margin: 0;
-        }
-        .cesium-credit-expand-link {
-          padding-left: 5px;
-          cursor: pointer;
-          text-decoration: underline;
-          color: #ffffff;
-        }
-        .cesium-credit-expand-link:hover {
-          color: #48b;
-        }
-        .cesium-credit-text {
-          color: #ffffff;
-        }
-        .cesium-credit-textContainer *,
-        .cesium-credit-logoContainer * {
-          display: inline;
-        }
-
-        .cesium-animation-rectButton .cesium-animation-buttonGlow {
-          filter: url(#animation_blurred);
-        }
-        .cesium-animation-rectButton .cesium-animation-buttonMain {
-          fill: url(#animation_buttonNormal);
-        }
-        .cesium-animation-buttonToggled .cesium-animation-buttonMain {
-          fill: url(#animation_buttonToggled);
-        }
-        .cesium-animation-rectButton:hover .cesium-animation-buttonMain {
-          fill: url(#animation_buttonHovered);
-        }
-        .cesium-animation-buttonDisabled .cesium-animation-buttonMain {
-          fill: url(#animation_buttonDisabled);
-        }
-        .cesium-animation-shuttleRingG .cesium-animation-shuttleRingSwoosh {
-          fill: url(#animation_shuttleRingSwooshGradient);
-        }
-        .cesium-animation-shuttleRingG:hover
-          .cesium-animation-shuttleRingSwoosh {
-          fill: url(#animation_shuttleRingSwooshHovered);
-        }
-        .cesium-animation-shuttleRingPointer {
-          fill: url(#animation_shuttleRingPointerGradient);
-        }
-        .cesium-animation-shuttleRingPausePointer {
-          fill: url(#animation_shuttleRingPointerPaused);
-        }
-        .cesium-animation-knobOuter {
-          fill: url(#animation_knobOuter);
-        }
-        .cesium-animation-knobInner {
-          fill: url(#animation_knobInner);
-        }
+ 
       `,
     ];
   }
